055e1446f8971aca038cc8751ad15c9db75b1d20
allangabr <allanmediogames@gmail.com>
2025-02-21 18:08:29 -0300

    Created entity author

---
+++ src/main/java/io/github/allangabrs/libraryapi/model/Author.java
@@ -0,0 +1,30 @@
+package io.github.allangabrs.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "author_name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "date_birth", nullable = false)
+    private LocalDate date_birth;
+
+    @Column(name = "nationality", length = 50, nullable = false)
+    private String nationality;
+
+}
