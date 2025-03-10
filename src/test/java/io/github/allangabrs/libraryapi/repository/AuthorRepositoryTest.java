554beea3dc0f090ebc11d28380819a899e22ab73
allangabr <allanmediogames@gmail.com>
2025-03-10 15:49:43 -0300

    More tests: List, Delete, DeleteById

--- src/test/java/io/github/allangabrs/libraryapi/repository/AuthorRepositoryTest.java
+++ src/test/java/io/github/allangabrs/libraryapi/repository/AuthorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,7 +17,7 @@ public class AuthorRepositoryTest {
     AuthorRepository repository;
 
     @Test
-    public void salvarTest(){
+    public void saveTest(){
         Author autor = new Author();
         autor.setName("allan");
         autor.setNationality("ingles");
@@ -27,7 +28,7 @@ public class AuthorRepositoryTest {
     }
 
     @Test
-    public void atualizarTest(){
+    public void updateTest(){
         var id = UUID.fromString("eeb3d705-c280-4597-aa2f-e8fac7dc24b6");
 
         Optional<Author> possibleAuthor = repository.findById(id);
@@ -44,4 +45,27 @@ public class AuthorRepositoryTest {
         }
     }
 
+    @Test
+    public void listTest(){
+        List<Author> list = repository.findAll();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    @Test
+    public void deleteByIdTest(){
+        var id = UUID.fromString("eeb3d705-c280-4597-aa2f-e8fac7dc24b6");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("5554ee52-94d3-45a0-a5fb-96e75ff9390a");
+        var author = repository.findById(id).get()  ;
+        repository.delete(author);
+    }
 }
